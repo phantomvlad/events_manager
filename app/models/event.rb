0454ce8c435cfcb7e15c3ab5c32c4c14156b2db7
Phantomvlad <mmvlad99@mail.ru>
2023-01-20 21:47:09 +0300

    minifix

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,9 +8,9 @@ class Event < ApplicationRecord
     attachable.variant :thumb, resize_to_limit: [300, 300]
     attachable.variant :big, resize_to_limit: [1200, 1200]
   end
+
   validates :event_avatar, content_type: %w[image/jpeg image/png image/gif],
     size: { less_than: 5.megabytes }
-
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
