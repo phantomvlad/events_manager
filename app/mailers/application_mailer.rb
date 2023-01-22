037d21b22b1a70606f2b1ab8991daab45e436714
Phantomvlad <mmvlad99@mail.ru>
2023-01-22 23:35:39 +0300

    test mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "help@event-manager-dance.ru"
+  layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'event-manager-dance.ru'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     address:         'smtp.mail.ru',
     port:            465,
-    domain:          'event-manager-dance.ru',
     user_name:       'help@event-manager-dance.ru',
     password:        Rails.application.credentials.dig(:mail_secret),
     authentication:  'plain',
