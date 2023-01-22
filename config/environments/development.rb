68a6917842515b2968145eeadc6aa66989bd8a5f
Phantomvlad <mmvlad99@mail.ru>
2023-01-22 21:32:45 +0300

    fix buggs again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,13 +72,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.ru',
-    domain: 'event-manager-dance.ru',
-    port: '465',
-    user_name: 'test@mail.ru',
-    password: 'ctjUJ3EGhTdyigYRPCNi',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
