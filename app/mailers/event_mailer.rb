0ca50f456427dfd6895222bc81e937b4731e6d61
Phantomvlad <mmvlad99@mail.ru>
2023-01-23 20:56:48 +0300

    fix buggs email

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "#{name} подписался на мероприятие '#{event.title}'"
+    mail to: event.user.email, subject: "#{@name} подписался на мероприятие '#{event.title}'"
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий в '#{event.title}' от @#{comment.user.name}"
+    mail to: email, subject: "Новый комментарий в '#{@event.title}' от @#{@comment.user.name}"
   end
 
   def photo(event, email, photo)
     @event = event
     @photo = photo
 
-    mail to: email, subject: "Новое фото в мероприятии '#{event.title}'"
+    mail to: email, subject: "Новое фото в мероприятии '#{@event.title}'"
   end
 end
