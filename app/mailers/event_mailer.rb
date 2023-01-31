8a2b61178efa39928e84421c4082035bd3dcd12e
Phantomvlad <mmvlad99@mail.ru>
2023-01-31 17:55:24 +0300

    Add full locate def subj

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,22 +4,22 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = subscription.event
 
-    mail to: @event.user.email, subject: "#{@name} подписался на мероприятие '#{@event.title}'"
+    mail to: @event.user.email, subject: default_i18n_subject(subscriber: @name, event_title: @event.title)
   end
 
   def comment(comment, email)
     @comment = comment
 
     if @comment.user.present?
-      mail to: email, subject: "Новый комментарий в '#{@comment.event.title}' от @#{@comment.user.name}"
+      mail to: email, subject: default_i18n_subject(user_comment: @comment.user.name, event_title: @comment.event.title)
     else
-      mail to: email, subject: "Новый комментарий в '#{@comment.event.title}' от @#{@comment.user_name}"
+      mail to: email, subject: default_i18n_subject(user_comment: @comment.user.name, event_title: @comment.event.title)
     end
   end
 
   def photo(email, photo)
     @photo = photo
 
-    mail to: email, subject: "Новое фото в мероприятии '#{@photo.event.title}'"
+    mail to: email, subject: default_i18n_subject(user_photo: @photo.user.name, event_title: @photo.event.title)
   end
 end
