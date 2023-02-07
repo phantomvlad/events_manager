dc57419410eda3f8fd373ce65ac75461f38e5573
PhantomVlad <mmvlad99@mail.ru>
2023-02-07 20:30:53 +0300

    Fix email

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -9,10 +9,7 @@ class EventMailer < ApplicationMailer
 
   def comment(comment, email)
     @comment = comment
-
-    @comment.user.present? ?
-      (mail to: email, subject: default_i18n_subject(user_comment: @comment.user.name, event_title: @comment.event.title)) :
-      (mail to: email, subject: default_i18n_subject(user_comment: @comment.user_name, event_title: @comment.event.title))
+    mail to: email, subject: default_i18n_subject(user_comment: @comment.user_name, event_title: @comment.event.title)
   end
 
   def photo(email, photo)
