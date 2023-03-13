a4536de8d1516232fa48e5f8000d88babc1ab66d
PhantomVlad <mmvlad99@mail.ru>
2023-03-13 18:23:39 +0300

    fix problem update event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_event, only: [:show, :edit, :destroy]
+  before_action :set_event, only: [:show, :edit, :destroy, :update]
   before_action :password_guard!, only: [:show]
 
   after_action :verify_authorized
@@ -91,5 +91,4 @@ class EventsController < ApplicationController
       render 'password_form'
     end
   end
-
 end
