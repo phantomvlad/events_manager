43dead1005bef906c66b3264b870c42aaad4111c
PhantomVlad <mmvlad99@mail.ru>
2023-03-28 18:22:57 +0300

    ActiveMailer add job

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,7 +7,7 @@ class SubscriptionsController < ApplicationController
     @new_subscription.user = current_user
 
     if @new_subscription.save
-      EventMailer.subscription(@new_subscription).deliver_later
+      MailerJob.perform_later(@event, @new_subscription)
 
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
